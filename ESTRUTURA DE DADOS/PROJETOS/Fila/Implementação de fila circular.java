8da4c270b6ae7bc86c76e3f2733a1661e10fa2c4
Fernanda Xdea <85658380+FernandaXdea@users.noreply.github.com>
2023-10-18 16:07:23 -0300

    Update Implementação de fila circular.java

--- ESTRUTURA DE DADOS/PROJETOS/Fila/Implementação de fila circular.java
+++ ESTRUTURA DE DADOS/PROJETOS/Fila/Implementação de fila circular.java
@@ -8,6 +8,64 @@ public class FilaCircular<T> impements Fila<T> {
     this.dados = (T[]) new Object [tamanho];
   }
   private int mover(int posição) {
-    return posição+1 ==dados.length ? 0 : posição+1;
+    return posição+1 ==d ados.length ? 0 : posição+1;
   }
-,
+
+@Override
+public boolean is Vazia() {
+  return topo== -1;
+}
+
+@Override
+public boolean isCheia() {
+  return !isVazia && mover (topo) == base; // na proxima movimentação, o topo está sobre a basse.
+}
+
+//FILA CIRCULAR- ADIÇÃO -> ADICIONA-SE PELO TOPO
+@Override
+public void adicionar(T valor) {
+  if (isCheia()) { //esta cheia?Dispara exceção
+    trow new IllegalStationException ("fila cheia");
+  }
+  topo = mover(topo);
+  dados [topo] = valor;
+}
+
+// FILA CIRCULAR -REMOÇÃO -> REMOVE-SE DA BASE
+@Override
+public T remover () {
+  if (isVazia()) {  //esta vazia?Dispara exceção
+    trow new IllegalStationException ("fila vazia");
+  }
+  T dado = dados[base];
+  if (base == topo) {//ultimo elemento?
+    limpar();
+  }else {
+    dados[base] = null;
+    base = mover(base);
+  }
+  return dado;
+  }
+
+// FILA CIRCULAR -LIMPEZA
+@Override 
+public void limpar() {
+  base = 0;
+  topo = -1;
+  for (int i=0; i <dados.length; i++){
+    dados[i] = null;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
