6a98036f1cdbc338e192054b368ec9a64c0fd7f9
Fernanda Xdea <85658380+FernandaXdea@users.noreply.github.com>
2023-10-18 15:34:20 -0300

    Update implementação.java

--- ESTRUTURA DE DADOS/PROJETOS/Fila/implementação.java
+++ ESTRUTURA DE DADOS/PROJETOS/Fila/implementação.java
@@ -1,1 +1,9 @@
-teste
+//Operaçoes iguais, conceito diferente
+//Interface da fila
+public interface Fila<T> {
+  void adicionar(T valor);
+  T remover();
+  boolean isCheia();
+  boolean is Vasia();
+  void limpar();
+}
