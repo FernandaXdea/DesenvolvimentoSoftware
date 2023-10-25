f8350a801ca3d9874fb585ab28143dc88acedd46
Fernanda Xdea <85658380+FernandaXdea@users.noreply.github.com>
2023-10-25 12:11:36 -0300

    Update Implementação lista estatica.java

--- ESTRUTURA DE DADOS/PROJETOS/Lista/Implementação lista estatica.java
+++ ESTRUTURA DE DADOS/PROJETOS/Lista/Implementação lista estatica.java
@@ -1,1 +1,28 @@
-teste.java
+//Estrutura incial
+public class ListaEstatica<T> implements Lista<T> {
+  privateT[] dados;
+  private int tamanho;
+  public ListaEstatica(int capacidade) {
+    this.dados =(T[]) new Object [capacidade];
+  }
+}
+
+//Informações
+@Override
+public int getTamanho(){ //tamanho da lista, a lista pode ter capacidade 10 mas o tamanho pode ser 5
+  return tamanho;
+}
+
+public int getCapacidade(){ //qnt maxima de elementos que posso incluir na lista
+  return dados.length;
+}
+
+@Override
+public boolean isVazia() {
+  return getTamanho() == 0;
+}
+
+@Override
+public boolean isCheia() { //qnd o tamanho for igual a capacidade
+  return getTamanho() == getCapacidade;
+}
